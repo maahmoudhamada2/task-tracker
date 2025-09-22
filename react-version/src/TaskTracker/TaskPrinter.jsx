983e71f7c7ad98b45eda340e01bf73036c5cc40e
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-09-23 00:47:34 +0300

    style(react-version): add base style to TaskPrinter component

--- react-version/src/TaskTracker/TaskPrinter.jsx
+++ react-version/src/TaskTracker/TaskPrinter.jsx
@@ -1,7 +1,11 @@
 import DeleteIcon from "./icons/DeleteIcon";
 
 export default function TaskPrinter({ setTasksList, task, setIsChanged }) {
-  const style = task.completed ? "text-gray-500 line-through" : null;
+  const titleStyle = task.completed ? "text-gray-500 line-through" : null;
+  const checkStyle = task.completed
+    ? "appearance-none bg-gray-500 rounded-sm"
+    : null;
+
   const handleClick = () => {
     task.completed = task.completed === true ? false : true;
     setTasksList((prev) =>
@@ -14,12 +18,19 @@ export default function TaskPrinter({ setTasksList, task, setIsChanged }) {
     setTasksList((prev) => prev.filter((elem) => elem.id !== task.id));
   };
   return (
-    <section>
-      <input onClick={handleClick} id={`task_${task.id}`} type="checkbox" />
-      <label className={`${style}`} htmlFor={`task_${task.id}`}>
+    <section className="w-full relative p-[0.5rem] flex items-center border-b-2 border-gray-200 my-[0.5rem]">
+      <input
+        className={`mr-[0.4rem] w-[17px] h-[17px] cursor-pointer ${checkStyle}`}
+        onClick={handleClick}
+        id={`task_${task.id}`}
+        type="checkbox"
+      />
+      <label className={`text-lg ${titleStyle}`} htmlFor={`task_${task.id}`}>
         {task.title}
       </label>
-      <button onClick={handleDelete}>
+      <button
+        className="absolute right-0 top-4 cursor-pointer"
+        onClick={handleDelete}>
         <DeleteIcon />
       </button>
     </section>

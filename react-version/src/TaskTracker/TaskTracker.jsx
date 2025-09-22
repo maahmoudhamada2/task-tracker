c863af6f622c870dbdb2f21151eaa3d8f97ef316
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-09-23 00:00:46 +0300

    feat(rect-version): add TaskTracker component

---
+++ react-version/src/TaskTracker/TaskTracker.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import TaskField from "./TaskField";
+import TaskPrinter from "./TaskPrinter";
+
+export default function TaskTracker() {
+  const [tasksList, setTasksList] = useState([]);
+  const [taskId, setTaskId] = useState(1);
+  const [, setIsChanged] = useState(false);
+
+  return (
+    <article>
+      <header>
+        <TaskField
+          setTasksList={setTasksList}
+          setTaskId={setTaskId}
+          id={taskId}
+        />
+      </header>
+      <main>
+        {tasksList
+          .map((elem) => (
+            <TaskPrinter
+              setTasksList={setTasksList}
+              key={elem.id}
+              task={elem}
+              setIsChanged={setIsChanged}
+            />
+          ))
+          .reverse()}
+      </main>
+    </article>
+  );
+}
